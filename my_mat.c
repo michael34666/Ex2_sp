0e4c78971df93c998f4dd2c4e09b985b5ed85e0e
michael34666 <118314640+michael34666@users.noreply.github.com>
2024-02-04 16:56:00 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -73,7 +73,7 @@ int initialize(int** mat,int length)
  int shortestPath(int** mat,int length,int i,int j)
  {
     FWA(mat,length);
-    if(mat[i][j] &&mat[i][j]!=INFINITY)
+    if(mat[i][j]!=0 &&mat[i][j]!=INFINITY)
     {
       return mat[i][j];
     }
