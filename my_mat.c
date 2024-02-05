d88a8862a6b472ecfc8c5b622e9a2ca0e61a5a86
michael34666 <118314640+michael34666@users.noreply.github.com>
2024-02-05 22:11:51 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -66,12 +66,11 @@ int initialize(int** mat,int length)
 
  int shortestPath(int** mat,int length,int i,int j)
  {
-    FWA(mat,length);
-    if(mat[i][j]!=0)
+   //FWA(mat,length);
+    if(havePath(mat,10,i,j))
     {
       return mat[i][j];
     }
 
     return -1;
  }
-
