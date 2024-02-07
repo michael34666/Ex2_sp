28bac0ac872d17b5b9c4bad41127cb92af3af711
michael34666 <118314640+michael34666@users.noreply.github.com>
2024-02-07 13:15:25 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -1,62 +1,88 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "my_mat.h"
+#include <stdbool.h>
+#include <math.h>
 
-int main()
-{
- char choose;
- int i=0;
- int j=0;
- 
- 
-    int mymat0[10][10];
- 
 
- for(int i=0;i<10;i++)
+
+//int fwarun=0;
+ 
+void initialize(int** mat,int length)
+ {
+    for(int i=0;i<length;i++)
     {
-     for(int j=0;j<10;j++)
+     for(int j=0;j<length;j++)
      {
-      mymat0[i][j]=0;
+      scanf("%d",&mat[i][j]);
+    
      }
     }
-
- 
- 
- do{
-
- scanf("%c",&choose);
-
- switch (choose)
- {
-  case 'A':
-  {
-   initialize(mymat0,10);
-   break; 
-  }
-  case 'B':
-  {
-    scanf("%d%d",&i,&j);
-    //char ans= havePath(mymat0,10,i,j);
-    printf("%s\n", havePath(mymat0,10,i,j)? "True":"False");
-    break; 
-  }
-  case 'C':
-  {
-    scanf("%d%d",&i,&j);
-    //int value=shortestPath(mymat0,10,i,j);
-    printf("%d\n",shortestPath(mymat0,10,i,j));
-    break; 
-  }
- default:
-    break;
   
-
  }
-
- }
-
- while(choose != 'D'&&choose != EOF);
  
-  
-return 0;
+ void FWA(int** mat,int length)
+{ 
+   int  copymat [length][length] ;
+
+   for(int i=0;i<length;i++)
+   {
+      for(int j=0;j<length;j++)
+      {
+         copymat[i][j]=mat[i][j];
+
+      }
+  }
+
+
+  for(int k=0;k<10;k++)
+   {
+      for(int i=0;i<10;i++)
+      {
+         for (int j= 0; j <10; j++)
+         {
+            
+             if((copymat[i][k]!=0 && copymat[k][j]!=0 && copymat[i][k]+copymat[k][j]<copymat[i][j])|| copymat[i][j]==0)
+             {
+               if(i!=j)
+               {
+              copymat[i][j]=copymat[i][k]+copymat[k][j];
+               }
+             }
+            
+         }
+         
+      }
+   }
+   
+   //fwarun=1;
+
 }
+ 
+ 
+ int havePath(int** mat,int length,int i,int j)
+ {
+   
+    FWA(mat,length);
+   
+    if(i==j)
+    {
+      return 0;
+    }
+      if (mat[i][j] > 0)
+    {
+        return 1;
+    }
+    return 0;
+ }
+
+ int shortestPath(int** mat,int length,int i,int j)
+ {
+  
+    FWA(mat,length);
+   
+    if(mat[i][j])
+    {
+      return mat[i][j];
+    }
+
+    return -1;
+ }
